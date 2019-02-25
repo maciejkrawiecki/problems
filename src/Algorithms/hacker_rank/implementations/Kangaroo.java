e685aaf839a62e10b63c23d64a742f1f617b7946
Maciej Krawiecki <m.a.krawiecki@gmail.com>
2019-02-25 16:43:28 +0100

    added kangaroo solution

---
+++ src/Algorithms/hacker_rank/implementations/Kangaroo.java
@@ -0,0 +1,17 @@
+package Algorithms.hacker_rank.implementations;
+
+public class Kangaroo {
+
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+
+        if (x1 < x2 && v1 <= v2) return "NO";
+        if (x2 < x1 && v2 <= v1) return "NO";
+
+
+        if ( ( (x2-x1) % (v1-v2) )  == 0 ){ return "YES";
+        }
+
+        return "NO";
+
+    }
+}
