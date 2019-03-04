0ef1a5611caa3eafe202b7edda03845e850388b0
Maciej Krawiecki <m.a.krawiecki@gmail.com>
2019-03-04 21:49:09 +0100

    added climbing the leaderboard solution (too slow a lil bit)

---
+++ src/Algorithms/hacker_rank/implementations/ClimbingTheLeaderboard.java
@@ -0,0 +1,41 @@
+package Algorithms.hacker_rank.implementations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClimbingTheLeaderboard {
+
+    static int[] climbingLeaderboard(int[] scores, int[] alice) {
+
+        int positions;
+
+        int[] result = new int[alice.length];
+
+
+        for (int i = 0; i < alice.length; i++) {
+
+            positions = 0;
+
+            for (int j = scores.length-1; j >= 0 ; j--) {
+
+                if (alice[i] > scores[j]) positions++;
+                if (alice[i] == scores [j]) positions++;
+
+            }
+
+            int len = scores.length - positions;
+
+            Set<Integer> set = new HashSet<>();
+
+            for (int j = 0; j < len; j++) {
+                set.add(scores[j]);
+            }
+
+            result[i] = set.size()+1;
+
+        }
+
+        return result;
+    }
+
+}
